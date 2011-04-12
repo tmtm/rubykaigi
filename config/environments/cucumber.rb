30eac8bb97c45b209007a14c4e4a3d282c7e3641
Ryunosuke SATO <tricknotes@yahoo.co.jp>
2011-04-12 12:53:48 +0900

    set how to notify deprecation

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -20,4 +20,6 @@ Rubykaigi::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+
+  config.active_support.deprecation = :stderr 
 end
