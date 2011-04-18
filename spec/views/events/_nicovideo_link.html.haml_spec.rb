9185267d96188bbf18925c04c4bcd9cf553b3ca0
Ryunosuke SATO <tricknotes@yahoo.co.jp>
2011-04-18 15:50:29 +0900

    fix spec

--- spec/views/events/_nicovideo_link.html.haml_spec.rb
+++ spec/views/events/_nicovideo_link.html.haml_spec.rb
@@ -6,13 +6,13 @@ describe '_nicovideo_link.html.haml' do
     before do
       render :partial =>'events/nicovideo_link', :locals => {:item_id => 'sm0000001', :link_text => t(:nicovideo)}
     end
-    it { response.should have_tag('a', t(:nicovideo)) }
+    it { response.should have_selector('a', :content => t(:nicovideo)) }
   end
 
   context 'with part number' do
     before do
       render :partial =>'events/nicovideo_link', :locals => {:item_id => 'sm0000001', :link_text => t(:nicovideo) + ' (Part 1)'}
     end
-    it { response.should have_tag('a', t(:nicovideo) + ' (Part 1)') }
+    it { response.should have_selector('a', :content => t(:nicovideo) + ' (Part 1)') }
   end
 end
