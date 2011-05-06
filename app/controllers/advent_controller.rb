621fec34590bcfa8b722ac429c240f8d976b3b8f
hibariya <celluloid.key@gmail.com>
2011-05-06 16:16:08 +0900

    translate event summary

--- app/controllers/advent_controller.rb
+++ app/controllers/advent_controller.rb
@@ -7,21 +7,26 @@ class AdventController < LocaleBaseController
   def index
     respond_to do |format|
       format.html
-      format.json { render :json => advent_to_json }
+      format.json { render :json => events_to_json }
     end
   end
 
   private
   
-  def load_events
-    require 'open-uri'
+    def load_events
+      require 'open-uri'
+  
+      ical = URI.parse(configatron.advent.ical_url).open(&:read)
+      @advent = RiCal.parse_string(ical).first
+      @events = @advent.events.map{|e| translate_event(e) }.sort_by(&:dtstart)
+    end
 
-    ical = URI.parse(configatron.advent.ical_url).open(&:read)
-    @advent = RiCal.parse_string(ical).first
-    @events = @advent.events.sort_by(&:dtstart)
-  end
+    def translate_event(event)
+      event.summary = I18n.t! event.summary.intern, :scope => :advent rescue event.summary
+      event
+    end
 
-    def advent_to_json
+    def events_to_json
       @events.map {|event|
         {:summary => event.summary,
          :dtstart => event.dtstart,
