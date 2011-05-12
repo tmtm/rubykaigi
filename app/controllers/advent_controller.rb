9633aa89794a5ea911982b0488e0f7dbf51fecb3
hibariya <celluloid.key@gmail.com>
2011-05-13 01:00:19 +0900

    add rss feed

--- app/controllers/advent_controller.rb
+++ app/controllers/advent_controller.rb
@@ -1,14 +1,79 @@
 class AdventController < LocaleBaseController
   layout_for_latest_ruby_kaigi
 
+  before_filter :load_events
+
   # GET /advent
   def index
-    AdventEvent.load params[:year]
     respond_to do |format|
       format.html
-      format.json { render :json => AdventEvent.to_json }
-      format.ics { render :text => AdventEvent.to_ical }
+      format.json { render :json => events_to_json }
+      format.ics  { render :text => events_to_ical }
+      format.rdf  { render :xml  => events_to_rdf }
     end
   end
+
+  private
+  
+    def load_events
+      AdventEvent.load params[:year]
+    end
+  
+    def events_to_json
+      AdventEvent.all.to_json
+    end
+
+    def events_to_ical
+      RiCal.Calendar do |cal|
+        AdventEvent.all.each do |e|
+          cal.event do |event|
+            event.summary     e.name
+            event.description e.description
+            event.dtstart     e.dtstart
+            event.dtend       e.dtstart
+            event.location    e.location
+          end
+        end
+      end.to_s
+    end
+
+    def events_to_rdf
+      page_url = advent_index_url params[:year], params[:locale]
+      xml = Builder::XmlMarkup.new
+      xml.instruct!
+      xml.rdf:RDF,   :xmlns        => "http://purl.org/rss/1.0/",
+                     :'xmlns:rdf'  => "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+                     :"xmlns:dc"   => "http://purl.org/dc/elements/1.1/",
+                     :'xmlns:foaf' => "http://xmlns.com/foaf/0.1/",
+                     :'xmlns:ical' => "http://www.w3.org/2002/12/cal/icaltzd#",
+                     :'xml:lang'   => "ja" do
+        xml.channel :'rdf:about' => page_url do
+          xml.title "RubyKaigi AdventCalender #{params[:year]}"
+          xml.link page_url
+          xml.description "RubyKaigi AdventCalender #{params[:year]}"
+          xml.items do
+            xml.rdf:Seq do
+              AdventEvent.all.each{|e| xml.rdf:li, :'rdf:resource' => page_url + "##{e.id}" }
+            end
+          end
+        end
+
+        AdventEvent.all.each do |e|
+          xml.item :about => page_with_link_url = page_url + "##{e.id}" do
+            xml.title e.name
+            xml.description e.description
+            xml.pubDate Time.now
+            xml.link page_with_link_url
+            xml.foaf:topic do
+              xml.ical:Vevent do
+                xml.ical:dtstart,  e.dtstart
+                xml.ical:dtend,    e.dtend
+                xml.ical:location, e.location
+              end
+            end
+          end
+        end
+      end
+    end
 
 end
--- app/models/advent_event.rb
+++ app/models/advent_event.rb
@@ -6,8 +6,8 @@ class AdventEvent
     attrs.symbolize_keys!
     self.id, self.description, self.location = attrs[:id], attrs[:description], attrs[:location]
     self.dtstart = Time.parse attrs[:dtstart] if attrs[:dtstart].present?
-    self.dtend = Time.parse attrs[:dtend] if attrs[:dtend].present?
-    self.name = attrs["name_#{I18n.locale}".intern]
+    self.dtend   = Time.parse attrs[:dtend]   if attrs[:dtend].present?
+    self.name    = attrs["name_#{I18n.locale}".intern]
   end
 
   def to_hash
@@ -19,7 +19,6 @@ class AdventEvent
 
   class << self
     extend ActiveSupport::Memoizable
-    delegate :to_json, :to => :all
 
     def load(year)
       @@raw_events ||= YAML.load File.open Rails.root.join('db', year.to_s, 'advent_events.yml'), 'r', &:read
@@ -29,21 +28,6 @@ class AdventEvent
       raw_events.map{|id, e| new e.merge(:id => id) }.sort_by &:dtstart
     end
 
-    def to_ical
-      RiCal.Calendar do |cal|
-        all.each do |e|
-          cal.event do |event|
-            event.summary     e.name
-            event.description e.description
-            event.dtstart     e.dtstart
-            event.dtend       e.dtstart
-            event.location    e.location
-          end
-        end
-      end.to_s
-    end
-
-    memoize :all, :to_ical
+    memoize :all
   end
-
 end
