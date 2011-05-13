0ae134ef0b216c85c8ef9063eab5e71be70d5545
Ryunosuke SATO <tricknotes@yahoo.co.jp>
2011-05-13 12:39:35 +0900

    use Localizer at Sponser

--- lib/ruby_kaigi2011/sponsor.rb
+++ lib/ruby_kaigi2011/sponsor.rb
@@ -3,6 +3,7 @@ require File.join(File.dirname(__FILE__), "yaml_loader")
 module RubyKaigi2011
   class Sponsor < OpenStruct
     extend YamlLoader
+    include Localizer
     def self.yaml_path
       Rails.root.join("db/2011/sponsors", name.gsub(/(RubyKaigi2011::|Sponsor)/, '').downcase)
     end
