08e4b0983c74a3478b1ccf961acf3c7371b12631
Kakutani Shintaro <shintaro@kakutani.com>
2011-05-17 20:45:14 +0900

    use UTC

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,8 +18,8 @@ ProductItem.create(:item_code => 'rk10_individual_sponsor', :unit_price => 20000
 
 rk11 = RubyKaigi.create(:year => 2011, :capacity => 1000, :party_capacity => 450)
 ProductItem.create(:item_code => 'rk11', :unit_price => 6000, :stock => 700, :ruby_kaigi => rk11,
-  :open_sale_at => DateTime.parse('2011-05-22 10:00:00 JST'), :ready_for_sale => true)
+  :open_sale_at => DateTime.parse('2011-05-22 01:00:00 UTC'), :ready_for_sale => true)
 ProductItem.create(:item_code => 'rk11_party', :unit_price => 5000, :stock => 200, :ruby_kaigi => rk11,
-  :open_sale_at => DateTime.parse('2011-05-22 10:00:00 JST'), :ready_for_sale => true)
+  :open_sale_at => DateTime.parse('2011-05-22 01:00:00 UTC'), :ready_for_sale => true)
 ProductItem.create(:item_code => 'rk11_individual_sponsor', :unit_price => 15000, :stock => 200, :ruby_kaigi => rk11,
-  :open_sale_at => DateTime.parse('2011-05-17 20:30:00 JST'), :ready_for_sale => true)
+  :open_sale_at => DateTime.parse('2011-05-17 11:30:00 UTC'), :ready_for_sale => true)
