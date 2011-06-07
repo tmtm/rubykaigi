c65a0c966163635c7fd93b3d23c36f2b925d512f
SHIDARA Yoji <dara@shidara.net>
2011-06-07 10:27:54 +0900

    "bundle exec rake" instead of "rake" on whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,7 @@
 # A standard output is recorded in the log file.
 # If an error occurs, cron will send mail to you.
 set :output, :standard => File.join(path, *%w{log cron.log})
+job_type :rake, "cd :path && RAILS_ENV=:environment bundle exec rake :task :output" # workaround for running rake with bundle exec
 
 every 1.hour do
   rake 'headline:fetch'
