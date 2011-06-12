a89b4c2f289eb37f1cb3fa85651fd1505cfa2745
hibariya <celluloid.key@gmail.com>
2011-06-12 12:12:59 +0900

    migrate rubyists
    - rubyists.identity_url    => authentications.uid
    - rubyists.twitter_user_id => authentications.uid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110611173030) do
+ActiveRecord::Schema.define(:version => 20110612025747) do
 
   create_table "authentications", :force => true do |t|
     t.string   "provider",   :null => false
